cc0747fc7e7846bc87524f9fe37afc83d8307316
Akos Gabriel <akos.gabriel@gmail.com>
2020-12-13 13:43:32 +0100

    store archive on nfs

--- arch.sh
+++ arch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir -p /tmp/archive
-find . -maxdepth 1 -name "part.*" -print0 |xargs -P4 -I% -r0 nice tar cfz /tmp/archive/%.tar.gz --null -T %
+mkdir -p archive
+find . -maxdepth 1 -name "part.*" -print0 |xargs -P4 -I% -r0 nice tar cfz archive/%.tar.gz --null -T %
 
