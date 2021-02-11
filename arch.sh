1b909aab4ed91f85ed45f087009cdc4554b02ac4
Akos Gabriel <akos.gabriel@gmail.com>
2021-02-11 16:43:44 +0100

    archive log added

--- arch.sh
+++ arch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p archive
-find . -maxdepth 1 -name "part.*" -print0 |xargs -P4 -I% -r0 nice tar cfz archive/%.tar.gz --null -T %
+find . -maxdepth 1 -name "part.*" -print0 |xargs -P4 -I% -r0 nice tar cfz archive/%.tar.gz --null -T % 2>arch.log
 
